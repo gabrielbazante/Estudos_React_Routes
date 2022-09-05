76a043c5cab727220e87e4d896e71c503b652926
Gabriel Bazante <gabrielbazante@MacBook-Air-de-Gabriel.local>
2022-09-05 20:38:09 -0300

    Criando estilos para as paginas

--- navegacao/src/components/layout/Menu.jsx
+++ navegacao/src/components/layout/Menu.jsx
@@ -3,7 +3,12 @@ import './Menu.css'
 
 const Menu = props => (
     <aside className="Menu">
-        Componente Menu
+        <nav>
+            <ul>
+                <li><a href="/">Inicio</a></li>
+                <li><a href="/">Sobre</a></li>
+            </ul>
+        </nav>
     </aside>
 )
 
